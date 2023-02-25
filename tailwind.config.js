3a08cb4f0779d6c80b0aabaedda044bf8cc566f7
Kamogelo <kammolop021@student.wethinkcode.co.za>
2023-02-25 14:20:33 +0200

    updated html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
   content: ['./src/**/*.{html,js}'],
   theme: {
+
     colors:{
         'gray-dark': '#273444',
         'gray': '#8492a6',
         'gray-light': '#d3dce6',
-    }
-
-    font-family:{
-        sans: ['Ariel', 'san-serif']
+        'Continue reading': '',
     }
 
     extend: {
